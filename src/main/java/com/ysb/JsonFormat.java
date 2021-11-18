dbaa0cfc2ae3ecf709588e621180abefdee8c94c
huangbangbang <huangbangbang@163.com>
2021-11-18 11:14:11 +0800

    阶段性的提交

---
+++ src/main/java/com/ysb/JsonFormat.java
@@ -0,0 +1,27 @@
+package com.ysb;
+
+import cn.hutool.json.JSONUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class JsonFormat {
+    public static void main(String[] args) throws JsonProcessingException {
+        User user1 = new User(1,"jack");
+        User user2 = new User(2,"mary");
+        ArrayList<User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.put(user1.getNo(),user1.getName());
+        hashMap.put(user2.getNo(),user2.getName());
+        System.out.println(new ObjectMapper().writeValueAsString(user1));
+        System.out.println(new ObjectMapper().writeValueAsString(user2));
+        System.out.println(new ObjectMapper().writeValueAsString(list));
+        System.out.println(new ObjectMapper().writeValueAsString(hashMap));
+        System.out.println("===================================================");
+        System.out.println(JSONUtil.toJsonStr(hashMap));
+    }
+}
--- src/main/java/com/ysb/StreamTest.java
+++ src/main/java/com/ysb/StreamTest.java
@@ -1,8 +1,17 @@
 package com.ysb;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamTest {
     public static void main(String[] args) {
 
-        
+        Stream.of("three", "four","three", "four","three", "four","three", "four","three", "four","one", "two", "three", "four")
+                .skip(10)
+                .filter(e -> e.length() > 3)
+                .peek(e -> System.out.println("Filtered value: " + e))
+                .map(String::toUpperCase)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .collect(Collectors.toList());
     }
 }
